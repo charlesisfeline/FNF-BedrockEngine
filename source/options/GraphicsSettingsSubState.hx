b557fefad0b797fe1c3cbd73efd961ddbe197246
Gui-iago <lucariothelast91@gmail.com>
2022-01-05 02:14:32 -0400

    you are going to compile... NOW!

--- source/options/GraphicsSettingsSubState.hx
+++ source/options/GraphicsSettingsSubState.hx
@@ -36,28 +36,27 @@ class GraphicsSettingsSubState extends BaseOptionsMenu
 		title = 'Graphics';
 		rpcTitle = 'Graphics Settings Menu'; //for Discord Rich Presence
 
-			var option:Option = new Option('Anti-Aliasing',
+		var option:Option = new Option('Anti-Aliasing',
 			'If unchecked, disables anti-aliasing, increases performance\nat the cost of sharper visuals.',
 			'globalAntialiasing',
 			'bool',
 			true);
 		option.showBoyfriend = true;
 		option.onChange = onChangeAntiAliasing; //Changing onChange is only needed if you want to make a special interaction after it changes the value
-	addOption(option);
+		addOption(option);
 
 		#if !html5 //Apparently other framerates isn't correctly supported on Browser? Probably it has some V-Sync shit enabled by default, idk
 		var option:Option = new Option('Framerate',
-		"Pretty self explanatory, isn't it?",
-		'framerate',
-		'int',
-		120);
-	addOption(option);
+			"Pretty self explanatory, isn't it?",
+			'framerate',
+			'int',
+			60);
+		addOption(option);
 		option.minValue = 60;
 		option.maxValue = 240;
 		option.displayFormat = '%v FPS';
 		option.onChange = onChangeFramerate;
-	#end
-
+		
 		//I'd suggest using "Low Quality" as an example for making your own option since it is the simplest here
 		var option:Option = new Option('Low Quality', //Name
 			'If checked, disables some background details,\ndecreases loading times and improves performance.', //Description
@@ -71,7 +70,7 @@ class GraphicsSettingsSubState extends BaseOptionsMenu
 		'maxOptimization',
 		'bool',
 		false);
-	addOption(option);
+		addOption(option);
 		
 		var option:Option = new Option('Screen Resolution',
 			"Size of the window (Changes will apply once leaving)",
