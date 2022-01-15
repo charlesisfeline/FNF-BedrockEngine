5f8be42f2716b6175fcb5a0111cb022911c4fd83
Tahir <84905346+TahirArch@users.noreply.github.com>
2022-01-16 00:38:16 +0300

    Update GameplaySettingsSubState.hx

--- source/options/GameplaySettingsSubState.hx
+++ source/options/GameplaySettingsSubState.hx
@@ -34,6 +34,13 @@ class GameplaySettingsSubState extends BaseOptionsMenu
 		title = 'Gameplay Settings';
 		rpcTitle = 'Gameplay Settings Menu'; //for Discord Rich Presence
 		
+		var option:Option = new Option('Marvelous Ratings',
+			'If unchecked, marvelous ratings will disappear.',
+			'marvelouses',
+			'bool',
+			true);
+		addOption(option);
+		
 		var option:Option = new Option('Controller Mode',
 			'Check this if you want to play with\na controller instead of using your Keyboard.',
 			'controllerMode',
