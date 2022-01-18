6b2a17c00ff45fe1d4c36a76879b7aab9832deef
Gui-iago <lucariothelast91@gmail.com>
2022-01-17 21:45:34 -0400

    make it a bit more accurate

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -991,6 +991,11 @@ class PlayState extends MusicBeatState
 			timeTxt.size = 24;
 			timeTxt.y += 3;
 		}
+		if (ClientPrefs.timeBarType == 'Song Name' && ClientPrefs.timeBarUi == 'Kade Engine')
+			{
+				timeTxt.size = 18;
+				timeTxt.y += 5;
+			}
 
 		var splash:NoteSplash = new NoteSplash(100, 100, 0);
 		grpNoteSplashes.add(splash);
