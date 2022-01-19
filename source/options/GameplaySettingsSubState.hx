b5f56b4c44aac10fd5cb851eeb1ab5188b08421a
Gui-iago <lucariothelast91@gmail.com>
2022-01-19 19:18:41 -0400

    Hide KE Acc

--- source/options/GameplaySettingsSubState.hx
+++ source/options/GameplaySettingsSubState.hx
@@ -58,7 +58,7 @@ class GameplaySettingsSubState extends BaseOptionsMenu
 	addOption(Option);
 
 		var Option = new Option('Kade Engine Accuracy', "If checked, the Accuracy will be more Complex, making it Harsher than before, good for Competitive Players", 'keAccuracy', 'bool', true);
-	addOption(Option);
+	//addOption(Option);
 
 		var option:Option = new Option('Marvelous Ratings', 'If unchecked, marvelous ratings will disappear.', 'marvelouses', 'bool', true);
 	addOption(option);
